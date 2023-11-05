7ab690552a98432c8e6963e7d1354b7282a689bc
ariaci <av@atomail.fr>
2023-11-05 02:38:55 +0100

    add redirections

--- symfony_init/src/Controller/HomeController.php
+++ symfony_init/src/Controller/HomeController.php
@@ -7,9 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
-    #[Route('/bonjour')]
+    //#[Route('/bonjour')]
     public function bonjour() {
         return new Response('<h1>Bonjour à toutes et à tous&nbsp;!<h1>');
     }
+
+    #[Route('/aurevoir')]
+    public function aurevoir() {
+        return $this->redirectToRoute('bonjour');
+    }
+
+    #[Route('/redirectToGoogle')]
+    public function redirectToGoogle() {
+        return $this->redirect('https://www.google.com');
+    }
+
 }
 
