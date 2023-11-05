11e02b7c225fc8cbe9fde2697985b0cac93ea089
ariaci <av@atomail.fr>
2023-11-05 02:24:34 +0100

    AbstractController

--- symfony_init/src/Controller/HomeController.php
+++ symfony_init/src/Controller/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController {
+class HomeController extends AbstractController {
     #[Route('/bonjour')]
     public function bonjour() {
-        return new Response('Bonjour à toutes et à tous!');
+        return new Response('<h1>Bonjour à toutes et à tous&nbsp;!<h1>');
     }
 }
 
