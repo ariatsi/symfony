2c9ea612d4bee91f291e5d2450d1762308c28fdf
ariaci <av@atomail.fr>
2023-11-13 03:05:25 +0100

    fin de Module 4

--- symfony_form/src/Controller/CustomerController.php
+++ symfony_form/src/Controller/CustomerController.php
@@ -5,22 +5,35 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Form\CustomerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
     #[Route('/customerform', name: 'customerform')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $customer = new Customer();
         $customerForm = $this->createForm(CustomerType::class, $customer);
+        $customerForm->handleRequest($request);
+
 
         // Ajouter la logique de traitement du formulaire si nécessaire...
+        if ($customerForm->isSubmitted() && $customerForm->isValid()) {
+            // Traitement des données soumises, par exemple, enregistrement dans la base de données.
+            // Pour l'instant, affichons simplement les données soumises :
+            dump($request->request->all());
+
+            // En réalité, ici, vous effectuerez des opérations telles que l'enregistrement dans la base de données.
+        }
+
 
         return $this->render('customer/index.html.twig', [
             'customerForm' => $customerForm->createView(),
         ]);
 
     }
+
+
 }
